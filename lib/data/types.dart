efa552d152aa85dc3546ba78dd9b51f1cd5771c0
tg21 <gautamtushar21@gmail.com>
2021-05-19 19:13:46 +0530

    educationData type and Experience data type

--- lib/data/types.dart
+++ lib/data/types.dart
@@ -9,3 +9,14 @@
 
   const ExperianceData({required this.organisation, required this.jobTitle, required this.timeSpan, this.info="",this.color = Colors.white70});
 }
+
+ class EducationData{
+  final String degree;
+  final String college;
+  final String timeSpan;
+  final Color color; //date span as string ,e.g- July 2019 - Aug 2021
+  final Color fontColor;
+
+  const EducationData({required this.degree, required this.college, required this.timeSpan, this.color = Colors.white70, this.fontColor = Colors.black});
+}
+
