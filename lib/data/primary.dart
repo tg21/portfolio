e4cd13be780df154d4720f80a11e013d1e92c066
tg21 <gautamtushar21@gmail.com>
2021-05-31 20:48:59 +0530

    primary data for tushar gautam

--- lib/data/primary.dart
+++ lib/data/primary.dart
@@ -14,7 +14,7 @@ const data = <dynamic, dynamic>{
 //   "user-image": "images/man.png"
 // };
 
-class HomeData{
+class HomeData {
   static const greeting = "hello";
   static const intro = "I'm Tushar Gautam";
   static const profile = "Software Engineer";
@@ -22,71 +22,50 @@ class HomeData{
   static const user_image = "assets/images/man.png";
 }
 
-
 class AboutData {
   static const bg_image = "assets/images/2about.webp";
   static const screenTitle = "About";
   static const about_summary =
-      """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis tempus efficitur. Integer iaculis urna sed diam dictum molestie.
-  Curabitur auctor, risus quis cursus semper, est diam varius mauris, The constraints that this widget passes to its child will adhere to the parent's constraints, so if the constraints are not large enough to satisfy the child's maximum intrinsic height, then the child will get less height than it otherwise would. Likewise, if the minimum height constraint is larger than the child's maximum intrinsic height, the child will be given more height than it otherwise wouldet dignissim augue justo ac massa. Integer a libero ac ligula ultrices auctor eu sed sapien. Sed sed cursus nibh. In sed interdum lorem. Nunc velit augue, sagittis in ex sed, bibendum cursus massa. Morbi ullamcorper mauris vel magna posuere mattis.""";
+      """I am Software Engineer. I have a degree in Computer Science and I am pursuing Master's degree in Computer Applications. I love programming and learning new technologies.
+I love every thing programming related but python, JavaScript, dart and c# are my favourite languages.
+I have worked on few of my personal projects""";
   static const List<ExperianceData> experience = [
     const ExperianceData(
-      organisation: "Sample Industries",
-      jobTitle: "Software Engineer",
-      timeSpan: "July 2019 - May 2021",
-      info:
-          "Worked as full-stack developer for random project and developed In-House tools",
+      organisation: "Tech Mahindra",
+      jobTitle: "Software Engineer Developer",
+      timeSpan: "July 2019 - present",
+      info: """\u2022 Understand requirements and develop prototypes.
+\u2022 Develop Frontend of Web-applications with angular,js
+\u2022 Develop Backend of Web applications with c# asp.net and MSSQL server.
+\u2022 Automate Tasks with python.
+\u2022 Refactor codebase for better maintainability and performance""",
       color: Colors.orangeAccent,
-    ),
-    const ExperianceData(
-        organisation: "Sample Industries",
-        jobTitle: "Software Engineer",
-        timeSpan: "July 2019 - May 2021",
-        info:
-            "Worked as full-stack developer for random project and developed In-House tools",
-        color: Colors.green),
-    const ExperianceData(
-        organisation: "Sample Industries",
-        jobTitle: "Software Engineer",
-        timeSpan: "July 2019 - May 2021",
-        info:
-            "Worked as full-stack developer for random project and developed In-House tools Worked as full-stack developer for random project and developed In-House tools Worked as full-stack developer for random project and developed In-House tools Worked as full-stack developer for random project and developed In-House tools Worked as full-stack developer for random project and developed In-House tools"),
-    const ExperianceData(
-      organisation: "Random Industries",
-      jobTitle: "Software Engineer",
-      timeSpan: "31 July 2019 - 14 May 2021",
-      color: Colors.deepPurpleAccent,
-      //info: "Worked as full-stack developer for Sample project and developed In-House tools"
     ),
   ];
 
   static const List<EducationData> education = [
     const EducationData(
-      degree: "Masters in Computer Applications",
-      college: "IGNOU",
-      timeSpan: "July 19- Huly 22",
-      color: Colors.teal,
+      degree: "Master of Computer Applications",
+      college: "Indira Gandhi National Open University\nDelhi",
+      timeSpan: "July 19 - August 22",
+      color: Colors.indigo,
       fontColor: Colors.white,
     ),
     const EducationData(
-      degree: "Masters in Computer Applications",
-      college: "IGNOU",
-      timeSpan: "July 19- Huly 22",
-      color: Colors.black45,
-      fontColor: Colors.white,
+      degree: "Bachelor of Computer Science",
+      college: """Bhaskaracharya College Of Applied Sciences
+Delhi University""",
+      timeSpan: "July 16- May 19",
+      color: Colors.amber,
+      fontColor: Colors.black87,
     ),
     const EducationData(
-      degree: "Masters in Computer Applications",
-      college: "IGNOU",
-      timeSpan: "July 19- Huly 22",
-    ),
-    const EducationData(
-      degree: "Masters in Computer Applications",
-      college: "IGNOU",
-      timeSpan: "July 19- Huly 22",
+      degree: "XII (Science)",
+      college: """Rajkiya Pratibha Vikas Vidhyalaya
+Dwarka, Delhi""",
+      timeSpan: "2016",
     ),
   ];
-  
 }
 
 class SkillsData {
@@ -100,50 +79,53 @@ class SkillsData {
     const SkillsFormat(
         skillName: "Python",
         expirenceTimeWithSkill: "4+ years",
-        details: "I have worked on few of my personal python projects. I have worked on few of my personal python projects. I have worked on few of my personal python projects. I have worked on few of my personal python projects. I have worked on few of my personal python projects. I have worked on few of my personal python projects. I have worked on few of my personal python projects.",
+        details:
+            "Python is one of my favourite langauges. It is my go to lanaguage to solve any problem with scriptable solution. Apart form using it to script almost every small problem with it, I have worked on few of my personal python projects that are a bit larger in scale ",
         certifications: [
-          const CertificationData(
-            certification: "SoloLearn Python Certification",
-            validaity: "lifetime",
-            url: "https://google.com",
-            image: "images/man.png",
-            infoCertificate: "Completed Python Course with Solo Learn",
-          ),
-          const CertificationData(
-            certification: "SoloLearn Conda Certification",
-            validaity: "lifetime",
-            url: "https://google.com",
-            image: "images/man.png",
-            infoCertificate: "Completed Conda Course with Solo Learn",
-          ),
+          // const CertificationData(
+          //   certification: "SoloLearn Python Certification",
+          //   validaity: "lifetime",
+          //   url: "https://google.com",
+          //   image: "images/man.png",
+          //   infoCertificate: "Completed Python Course with Solo Learn",
+          // ),
+          // const CertificationData(
+          //   certification: "SoloLearn Conda Certification",
+          //   validaity: "lifetime",
+          //   url: "https://google.com",
+          //   image: "images/man.png",
+          //   infoCertificate: "Completed Conda Course with Solo Learn",
+          // ),
         ],
         projects: [
           const ProjectsData(
             project: "Python On Pails",
             duration: "2018 - Current",
-            aboutProject: "Python On Pails is an OpenSource lightweight web development framework that is designed to speed up development process",
-            image: "images/man.png",
-            url: "https://github.com",
+            aboutProject:
+                "Python On Pails is an OpenSource lightweight web development framework that is designed to speed up development process",
+            //image: "assets/images/others/pyop.webp",
+            sourceUrl: "https://github.com/tg21/python-on-pails",
           ),
           const ProjectsData(
             project: "WiBuster",
             duration: "2018 - Current",
-            aboutProject: "WiBuseter is an opesource website enumerator made with python3",
-            image: "images/man.png",
-            url: "https://github.com",
+            aboutProject:
+                "WiBuster is an opesource website enumerator made with python3. It can enumerate even those websites that respond with code 200 even when resource is not present",
+            //image: "assets/images/others/wibuster.webp",
+            sourceUrl: "https://github.com/tg21/wibuster",
           ),
-        ]
-        ),
+        ]),
     const SkillsFormat(
         skillName: "Flutter",
         expirenceTimeWithSkill: "1+ years",
-        details: "I have worked on few of my personal Flutter projects.",
+        details: """I have been working with flutter for more than a year, and I think this is one of the best frameworks available for cross-platform development, backed by big commmunity and all the benefits of dart langauage are major plus points.
+I have worked with two major flutter projects.""",
         certifications: [
           const CertificationData(
             certification: "App Brewery Introduction to flutter Certification",
             validaity: "lifetime",
             url: "https://google.com",
-            image: "images/man.png",
+            //image: "images/man.png",
             infoCertificate: "Completed Flutter Course with App Brewery",
           ),
           const CertificationData(
@@ -158,20 +140,21 @@ class SkillsData {
           const ProjectsData(
             project: "The Last Dino Run",
             duration: "2020 - 2021",
-            aboutProject: "An android game based on Chrome's game made with flutter from scratch without using any game engines like flame or unity",
+            aboutProject:
+                "An android game based on Chrome's game made with flutter from scratch without using any game engines like flame or unity",
             image: "images/man.png",
             url: "https://github.com",
           ),
           const ProjectsData(
             project: "This Website",
             duration: "2021 - 2021",
-            aboutProject: "WiBuseter is an opesource website enumerator made with python3",
-            image: "images/man.png",
+            aboutProject:
+                "WiBuseter is an opesource website enumerator made with python3",
+            //image: "images/man.png",
             url: "https://github.com",
           ),
-        ]
-        ),
-        const SkillsFormat(
+        ]),
+    const SkillsFormat(
         skillName: "Angular",
         expirenceTimeWithSkill: "1+ years",
         details: "I have worked on few of my personal Flutter projects.",
@@ -195,21 +178,22 @@ class SkillsData {
           const ProjectsData(
             project: "The Last Dino Run",
             duration: "2020 - 2021",
-            aboutProject: "An android game based on Chrome's game made with flutter from scratch without using any game engines like flame or unity",
+            aboutProject:
+                "An android game based on Chrome's game made with flutter from scratch without using any game engines like flame or unity",
             image: "images/man.png",
             url: "https://github.com",
           ),
           const ProjectsData(
             project: "This Website",
             duration: "2021 - 2021",
-            aboutProject: "WiBuseter is an opesource website enumerator made with python3",
+            aboutProject:
+                "WiBuseter is an opesource website enumerator made with python3",
             image: "images/man.png",
             url: "https://github.com",
             sourceUrl: "https://tg21.github.io",
           ),
-        ]
-        ),
-        const SkillsFormat(
+        ]),
+    const SkillsFormat(
         skillName: "React-Native",
         expirenceTimeWithSkill: "1+ years",
         details: "I have worked on few of my personal Flutter projects.",
@@ -233,39 +217,39 @@ class SkillsData {
           const ProjectsData(
             project: "The Last Dino Run",
             duration: "2020 - 2021",
-            aboutProject: "An android game based on Chrome's game made with flutter from scratch without using any game engines like flame or unity",
+            aboutProject:
+                "An android game based on Chrome's game made with flutter from scratch without using any game engines like flame or unity",
             image: "images/man.png",
             //url: "https://github.com",
           ),
           const ProjectsData(
             project: "This Website",
             duration: "2021 - 2021",
-            aboutProject: "WiBuseter is an opesource website enumerator made with python3",
+            aboutProject:
+                "WiBuseter is an opesource website enumerator made with python3",
             image: "images/man.png",
             sourceUrl: "https://github.com",
           ),
-        ]
-        ),
+        ]),
   ];
 }
 
-class ContactData{
+class ContactData {
   static const bg_image = "assets/images/6contact.webp";
   static const List<ContactClass> contacts = [
     const ContactClass(
-      contactName : "Gmail",
-      icon: FontAwesomeIcons.envelope,
-      url: "mailto:gautamtushar21@gmail.com",
-      iconColor: Colors.redAccent
-    ),
+        contactName: "Gmail",
+        icon: FontAwesomeIcons.envelope,
+        url: "mailto:gautamtushar21@gmail.com",
+        iconColor: Colors.redAccent),
     const ContactClass(
-      contactName : "Twitter",
+      contactName: "Twitter",
       icon: FontAwesomeIcons.twitter,
       url: "https://twitter.com/CoderWhoLived",
       iconColor: Colors.blueAccent,
     ),
     const ContactClass(
-      contactName : "StackOverflow",
+      contactName: "StackOverflow",
       icon: FontAwesomeIcons.stackOverflow,
       url: "https://twitter.com/CoderWhoLived",
       iconColor: Colors.deepOrange,
@@ -273,7 +257,7 @@ class ContactData{
       bgColor: Colors.black87,
     ),
     const ContactClass(
-      contactName : "Hackerrank",
+      contactName: "Hackerrank",
       icon: FontAwesomeIcons.hackerrank,
       url: "https://twitter.com/CoderWhoLived",
       iconColor: Colors.white,
@@ -281,27 +265,34 @@ class ContactData{
       bgColor: Colors.green,
     ),
     const ContactClass(
-      contactName : "StackOverflow",
+      contactName: "StackOverflow",
       icon: FontAwesomeIcons.stackOverflow,
       url: "https://twitter.com/CoderWhoLived",
       iconColor: Colors.deepOrange,
       textColor: Colors.white70,
       bgColor: Colors.black87,
     ),
-    
   ];
 }
 
-class BlogData{
+class BlogData {
   static const bg_image = "assets/images/5litrature.webp";
   static const primaryColor = Colors.yellow;
   static const List<BlogClass> blogList = [
-    const BlogClass(name: "How To Fix a bulb without moving a muscle 💡", url: "https://google.com",details: """When the exception was thrown, this was the stack
+    const BlogClass(
+        name: "How To Fix a bulb without moving a muscle 💡",
+        url: "https://google.com",
+        details: """When the exception was thrown, this was the stack
 C:/b/s/w/ir/cache/builder/src/out/host_debug/dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/errors.dart 236:49      throw_
 C:/b/s/w/ir/cache/builder/src/out/host_debug/dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/errors.dart 29:3        assertFailed
 packages/flutter/src/widgets/container.dart 274:42                                                                             new
-packages/portfolio/screens/blog.dart 62:32""" ),
-    const BlogClass(name: "Why reddit is the worst platform for anything", url: "https://reddit.com",bgColor: Colors.green,textColor: Colors.white,details:"""When the exception was thrown, this was the stack
+packages/portfolio/screens/blog.dart 62:32"""),
+    const BlogClass(
+        name: "Why reddit is the worst platform for anything",
+        url: "https://reddit.com",
+        bgColor: Colors.green,
+        textColor: Colors.white,
+        details: """When the exception was thrown, this was the stack
 C:/b/s/w/ir/cache/builder/src/out/host_debug/dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/errors.dart 236:49      throw_
 C:/b/s/w/ir/cache/builder/src/out/host_debug/dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/errors.dart 29:3        assertFailed
 packages/flutter/src/widgets/container.dart 274:42                                                                             new
@@ -309,8 +300,7 @@ packages/portfolio/screens/blog.dart 62:32"""),
   ];
 }
 
-
-class ArtData{
+class ArtData {
   static const bg_image = 'assets/images/4art.webp';
   static const List<String> photos = [
     'assets/images/art/art1.webp',
