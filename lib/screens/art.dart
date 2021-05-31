d67572574567bc9e3aa062e816a886eec16ab1a0
tg21 <gautamtushar21@gmail.com>
2021-05-31 20:47:00 +0530

    removed entry.dart

--- lib/screens/art.dart
+++ lib/screens/art.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:entry/entry.dart';
+// import 'package:entry/entry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
