1b8bd0b6448b44084d25f1bec56742bbe574b476
tg21 <tg21@users.noreply.github.com>
2021-05-31 15:28:03 +0000

    Update static to output generated at e4cd13b

--- assets/images/others/jpg_to_webp.py
+++ assets/images/others/jpg_to_webp.py
@@ -4,8 +4,8 @@ import os
 import glob
 files = os.listdir()
 for file in files:
-     if(not file.endswith('.webp') or file.startswith('low_')):
+     if(not file.endswith('.png') or file.startswith('low_')):
         continue
      im = Image.open(file)
      im = im.convert('RGB')
-     im.save('low_'+file.split('.')[0]+'.webp','webp',optimize=True,quality=0.01)
+     im.save(file.split('.')[0]+'.webp','webp',optimize=True,quality=1)
