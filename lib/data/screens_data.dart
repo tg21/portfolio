8a282854c28d23cb596d9d9a43069675b8296d54
tg21 <gautamtushar21@gmail.com>
2021-06-01 21:54:10 +0530

    changed screen icons

--- lib/data/screens_data.dart
+++ lib/data/screens_data.dart
@@ -28,13 +28,13 @@ class ScreenData{
 //mainly data for actionbar
 var screensData = [
  ScreenData(
-    icon:Icons.house_outlined,
+    icon:FontAwesomeIcons.fortAwesome,
     tooltip: "Home",
     widget: HomeScreen(),
-    primaryColor: Color(0xFF3D173F),
+    primaryColor: Colors.indigo,
      ),
   ScreenData(
-    icon:Icons.details_outlined,
+    icon:FontAwesomeIcons.solidUser,
     tooltip: "About",
     widget: AboutScreen(),
     primaryColor: Colors.redAccent,
@@ -42,109 +42,27 @@ var screensData = [
   
      
   ScreenData(
-    icon:Icons.developer_mode_outlined,
+    icon:FontAwesomeIcons.solidFileCode,
     tooltip: "Skills",
     widget: SkillScreen(),
     primaryColor: Colors.pink,
      ),
   ScreenData(
-    icon:FontAwesomeIcons.blog,
+    icon:FontAwesomeIcons.scroll,
     tooltip: "Blog",
     widget: BlogScreen(),
-    primaryColor: Colors.yellow,
+    primaryColor: Colors.deepOrangeAccent,
      ),
   ScreenData(
-    icon:FontAwesomeIcons.paintBrush,
+    icon:FontAwesomeIcons.palette,
     tooltip: "Art",
     widget: ArtScreen(),
     primaryColor: Colors.green,
      ),
   ScreenData(
-    icon:Icons.contact_mail_outlined,
+    icon:FontAwesomeIcons.solidIdCard,
     tooltip: "contact",
     widget: ContactScreen(),
     primaryColor: Colors.blueAccent,
      ),
-     
-   
-     
-  // new ScreenData(
-  //   icon:Icons.house_outlined,
-  //   tooltip: "Home",
-  //   widget: HomeScreen(),
-  //   primaryColor: Color(0xFF3D173F),
-  //    ),
-     
-  // new ScreenData(
-  //   icon:Icons.house_outlined,
-  //   tooltip: "About",
-  //   widget: AboutScreen(),
-  //   primaryColor: Color(0xFF3D173F),
-  //    ),
-     
-  // new ScreenData(
-  //   icon:Icons.house_outlined,
-  //   tooltip: "Showcase",
-  //   widget: HomeScreen(),
-  //   primaryColor: Color(0xFF3D173F),
-  //    ),
-     
-  // new ScreenData(
-  //   icon:Icons.house_outlined,
-  //   tooltip: "Home",
-  //   widget: HomeScreen(),
-  //   primaryColor: Color(0xFF3D173F),
-  //    ),
-     
-  // new ScreenData(
-  //   icon:Icons.house_outlined,
-  //   tooltip: "Home",
-  //   widget: HomeScreen(),
-  //   primaryColor: Color(0xFF3D173F),
-  //    ),
-     
-  // <dynamic,dynamic>{
-  //   "icon" : Icons.house_outlined,
-  //   "tooltip" : "Home",
-  //   "widget" : HomeScreen(),
-  //   "primaryColor" : Color(0xFF3D173F),
-  // },
-  // <dynamic,dynamic>{
-  //   "icon" : Icons.details_outlined,
-  //   "tooltip" : "About",
-  //   "widget" : AboutScreen(),
-  //   "primaryColor" :Color(0xFF8A1122),
-  // },
-  // const <dynamic,dynamic>{
-  //   "icon" : Icons.developer_mode_outlined,
-  //   "tooltip" : "Projects",
-  //   "widget" : "images/3showcase.webp",
-  //   "primaryColor" : Color(0xFFF59094),
-  // },
-  // const <dynamic,dynamic>{
-  //   "icon" : Icons.brush_outlined,
-  //   "tooltip" : "Drawings",
-  //   "widget" : "images/4art.webp",
-  //   "primaryColor" : Colors.green,
-  // },
-  // const <dynamic,dynamic>{
-  //   "icon" : Icons.menu_book_outlined,
-  //   "tooltip" : "Poems",
-  //   "widget" : "images/5litrature.webp",
-  //   "primaryColor" : Color(0xFF0484D6),
-  // },
-  // const <dynamic,dynamic>{
-  //   "icon" : Icons.contact_mail_outlined,
-  //   "tooltip" : "Contact",
-  //   "widget" : "images/6contact.webp",
-  //   "primaryColor" : Colors.pinkAccent,
-  // },
-  // const <dynamic,dynamic>{
-  //   "icon" : Icons.rss_feed,
-  //   "tooltip" : "Place Holder",
-  //   "widget" : "images/7misc.webp",
-  //   "primaryColor" : Color(0xFF5E5E5E),
-  // },
-
-
 ];
